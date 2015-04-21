269983a1a58fb71fc36f3c5bb3b364a8f4d058d0
andwah <anders.wahlgren@societyobjects.com>
2015-04-21 18:02:54 +0200

    New look

--- package.bat
+++ package.bat
@@ -1,37 +1,54 @@
 @echo off
 SETLOCAL
+SET output=artifacts
 
 REM Create empty folder where will will create the file structure to be packed (zipped)
-IF EXIST "%~dp0temp\NUL" (
-	rd "%~dp0temp" /S /Q
+IF EXIST "%~dp0temp\" (
+	rd "%~dp0temp" /S /Q || (GOTO ERROR)
 ) else (
- md "%~dp0temp"
+ md "%~dp0temp" || (GOTO ERROR)
 )
 
-REM Prepare Executables
-md "%~dp0temp\app"
-xcopy "%~dp0bin\Debug\*.*" "%~dp0temp\app"
+IF EXIST "%~dp0%output%\" (
+	rd "%~dp0%output%" /S /Q || (GOTO ERROR)
+)
 
-REM Prepare Content
-md "%~dp0temp\wwwroot"
-xcopy "%~dp0src\AppStoreClient\wwwroot" "%~dp0temp\wwwroot" /s /e
+
+REM Get projectname from current folder name
+for %%a in (.) do set projectname=%%~na
+
+REM Prepare Executables
+IF EXIST "%~dp0bin\Debug\*.exe" (
+	md "%~dp0temp\app" || (GOTO ERROR)
+	xcopy "%~dp0bin\Debug\*.*" "%~dp0temp\app" || (GOTO ERROR)
+) else (
+	echo No Executable found
+	GOTO ERROR
+)
+
+
+REM Prepare wwwroot
+md "%~dp0temp\wwwroot" || (GOTO ERROR)
+xcopy "%~dp0src\%projectname%\wwwroot" "%~dp0temp\wwwroot" /s /e || (GOTO ERROR)
 
 REM Copy icon and config
-xcopy "%~dp0src\AppStoreClient\package\*.png" "%~dp0temp"
-xcopy "%~dp0src\AppStoreClient\package\*.config" "%~dp0temp"
+xcopy "%~dp0src\%projectname%\package\*.png" "%~dp0temp" || (GOTO ERROR)
+xcopy "%~dp0src\%projectname%\package\*.config" "%~dp0temp" || (GOTO ERROR)
 
 REM Get folder name for the zip name
-for %%a in ("%~dp0.") do set currentfolder=%%~na
+REM for %%a in ("%~dp0.") do set currentfolder=%%~na
 
-
-IF NOT EXIST "%~dp0packages" (
- md "%~dp0packages"
+IF NOT EXIST "%~dp0%output%" (
+ md "%~dp0%output%" || (GOTO ERROR)
 )
 
 REM Zipp-it
-CD "%~dp0temp"
+CD "%~dp0temp" || (GOTO ERROR)
 
-zip -r "..\packages\%currentfolder%" *.*  || (GOTO ERROR)
+zip -r "..\%output%\%projectname%" *.*  || (GOTO ERROR)
+
+Echo .
+Echo Package Successfully created (%output%\%projectname%.zip)
 
 REM TODO Jump back to previous dir
 GOTO :CLEANUP
@@ -39,8 +56,13 @@ GOTO :CLEANUP
 :ERROR
 
 :CLEANUP
+
 CD "%~dp0"
-rd "%~dp0temp" /S /Q
+
+IF EXIST "%~dp0temp\" (
+	rd "%~dp0temp" /S /Q 
+) 
+
 
 :END
 
--- run.bat
+++ run.bat
@@ -1,2 +1,19 @@
-@ECHO OFF
-star --resourcedir="%~dp0src\AppStoreClient\wwwroot" "%~dp0bin\Debug\AppStoreClient.exe"
+@echo off
+
+SETLOCAL
+
+REM Get projectname from current folder name
+for %%a in (.) do set projectname=%%~na
+
+REM Get Executable
+FOR %%i IN ("%~dp0bin\debug\*.exe") DO ( 
+SET executable=%%i
+)
+
+IF DEFINED executable (
+ star -d=default --resourcedir="%~dp0src\%projectname%\wwwroot" "%executable%"
+) ELSE (
+ ECHO executable was not found.
+)
+
+
