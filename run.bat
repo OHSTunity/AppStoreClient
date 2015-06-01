c6d97c738ab53aab47491235b4455c6ca12b7917
Marcin Warpechowski <warpech@gmail.com>
2015-06-01 11:05:52 +0200

    "projectname" variable was only resolved correctly in the current directory (but not when run.bat was called from an external directory)

--- run.bat
+++ run.bat
@@ -2,18 +2,14 @@
 
 SETLOCAL
 
-REM Get projectname from current folder name
-for %%a in (.) do set projectname=%%~na
-
 REM Get Executable
 FOR %%i IN ("%~dp0bin\debug\*.exe") DO ( 
 SET executable=%%i
 )
 
 IF DEFINED executable (
- star -d=default --resourcedir="%~dp0src\%projectname%\wwwroot" "%executable%"
+ star -d=default --resourcedir="%~dp0src\AppStoreClient\wwwroot" "%executable%"
 ) ELSE (
  ECHO executable was not found.
 )
-
 
